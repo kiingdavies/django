fb1ae30f2bd275dce908cc048a44f4aa207ce59e
kiingdavies <daviesadedeji007@gmail.com>
2020-02-29 13:05:28 +0100

    Django Setup

---
+++ doc.py
@@ -0,0 +1,11 @@
+# STEP 1
+# run: pipenv install django==2.1 (version 2.1) in terminal
+# run: pipenv shell (to activate virtual environment)
+# run: django-admin startproject vidly (vidly is the folder name)
+# open manage.py then click python interpreter on bottom left
+# change to the py interpreter with the virtual env name in terminal ( & C:/Users/DAVIES/.virtualenvs/django-TF6dU37Q/Scripts/python.exe c:/django/doc.py ). Run: print("hello")
+# run: & C:/Users/DAVIES/.virtualenvs/django-TF6dU37Q/Scripts/python.exe c:/django/vidly/manage.py runserver
+# click on the server address in terminal to open in browser
+
+#STEP 2
+
